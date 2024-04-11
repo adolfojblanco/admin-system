5404d5c46a71bb030fefd34d6ef98436c26518d7
Adolfo Blanco <ajb156@outlook.com>
2024-04-11 21:13:44 +0200

    update settings

--- backend/app/settings/base.py
+++ backend/app/settings/base.py
@@ -120,7 +120,7 @@ TIME_INPUT_FORMATS = "%H:%M:%S"
 
 STATIC_URL = 'backend/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'backend/staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles/'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
